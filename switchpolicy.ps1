d58fea6700d5e4872b75d2e0e53f0339d1666f5b
Sherryl Manalo <smanalo@microsoft.com>
2018-02-27 14:21:34 +0100

    beautified

--- switchpolicy.ps1
+++ switchpolicy.ps1
@@ -1,28 +1,33 @@
-﻿#Prompt for switch 
-$whereto = ""
-$whereto = Read-Host -Prompt 'Switch to this API (westeu,northeu)'
+﻿# Parameters // Change according to your settings
+$ResourceGroupName = 'acf-api-mgmt' # Name of the RG which contains the APIM service
+$APIName = 'northeu.azurewebsites.net' # Name of your API
+$PolicyFileNameFirst = 'policyfile_west.txt' # Policyfile to switch to or from
+$PolicyFileNameSecond = 'policyfile_north.txt' # Policyfile to switch to or from
+$PSScriptRoot = "C:\Users\smanalo\Source\Repos\PingAPIMgmt\" # Working directory, make sure your files are in this one
+
+
+#Prompt for switch 
+$whereto = ''
+$whereto = Read-Host -Prompt 'Switch to this API (first,second)'
 Write-Host "You chose" $whereto
  
 
 # Get context and set URL
-$ApiMgmtObj = Get-AzureRmApiManagement -ResourceGroupName acf-api-mgmt
+$ApiMgmtObj = Get-AzureRmApiManagement -ResourceGroupName $ResourceGroupName
 $ApimUrl = $ApiMgmtObj.RuntimeUrl
 $ApimService = $ApiMgmtObj.Name
 
-$ApiMgmtContext = New-AzureRmApiManagementContext -ResourceGroupName acf-api-mgmt -ServiceName $ApimService
-$switchapi = Get-AzureRmApiManagementApi -Context $ApiMgmtContext -Name "northeu.azurewebsites.net"
+$ApiMgmtContext = New-AzureRmApiManagementContext -ResourceGroupName $ResourceGroupName -ServiceName $ApimService
+$switchapi = Get-AzureRmApiManagementApi -Context $ApiMgmtContext -Name $APIName
 
 # Read Policy File
-$PolicyFileNameWest = 'policyfile_west.txt'
-$PolicyFileNameNorth = 'policyfile_north.txt'
-if ($whereto -eq 'westeu') {$PolicyFileName = $PolicyFileNameWest; 
-        Write-Host "Please wait, switching to Western Europe"; }
-    elseif ($whereto -eq 'northeu') {$PolicyFileName = $PolicyFileNameNorth; 
-        Write-Host "Please wait, switching to North Europe"; }
-    else { Write-Host "You did not enter a valid region"; 
+if ($whereto -eq 'first') {$PolicyFileName = $PolicyFileNameFirst; 
+        Write-Host "Please wait, switching to First API"; }
+    elseif ($whereto -eq 'second') {$PolicyFileName = $PolicyFileNameSecond; 
+        Write-Host "Please wait, switching to Second API"; }
+    else { Write-Host "You did not enter a valid option"; 
        exit; }
 
-$PSScriptRoot = "C:\Users\smanalo\Source\Repos\PingAPIMgmt\"
 
 $PolicyFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $PolicyFileName))
 
