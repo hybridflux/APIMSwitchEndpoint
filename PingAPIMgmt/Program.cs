a096ec6a1626d70347a5a1b4cb530e3b058f60a2
Sherryl Manalo <smanalo@microsoft.com>
2018-02-27 10:27:12 +0100

    clean-up

--- PingAPIMgmt/Program.cs
+++ PingAPIMgmt/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
+using System.Threading;
+
+
 
 namespace PingAPIMgmt
 {
+    public class ConsumeEventSync
+    {
+        public void GetAllEventData() //Get All Events Records  
+        { 
+            using (var client = new WebClient()) //WebClient  
+            {
+                client.Headers.Add("Content-Type:application/json"); //Content-Type  
+                client.Headers.Add("Accept:application/json");
+                client.Headers.Add("Ocp-Apim-Subscription-Key:57fa26836bc3482195f6e987a564f102");
+                var result = client.DownloadString("https://apimtestfeoifkldf.azure-api.net/northeu/api/HttpTriggerCSharp1"); //URI  
+                DateTime currentDateTime = DateTime.Now;
+
+                Console.WriteLine("{0} - {1}",currentDateTime,Environment.NewLine + result);
+            }
+    
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            ConsumeEventSync objsync = new ConsumeEventSync();
+            while (true)
+            {
+                if (cts.IsCancellationRequested)
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+
+                var t = Task.Factory.StartNew(async delegate
+                {
+                    await Task.Delay(1000);
+                    objsync.GetAllEventData();
+                }).Unwrap();
+
+
+
+            }
+
+            System.Console.ReadLine();
+
+            System.Console.CancelKeyPress += (s, e) =>
+
+            {
+
+                e.Cancel = true;
+
+                cts.Cancel();
+
+                Console.WriteLine("Exiting...");
+
+            };
+            
         }
     }
 }
---
+++ switchpolicy.ps1
@@ -0,0 +1,34 @@
+﻿#Prompt for switch 
+$whereto = ""
+$whereto = Read-Host -Prompt 'Switch to this API (westeu,northeu)'
+Write-Host "You chose" $whereto
+ 
+
+# Get context and set URL
+$ApiMgmtObj = Get-AzureRmApiManagement -ResourceGroupName acf-api-mgmt
+$ApimUrl = $ApiMgmtObj.RuntimeUrl
+$ApimService = $ApiMgmtObj.Name
+
+$ApiMgmtContext = New-AzureRmApiManagementContext -ResourceGroupName acf-api-mgmt -ServiceName $ApimService
+$switchapi = Get-AzureRmApiManagementApi -Context $ApiMgmtContext -Name "northeu.azurewebsites.net"
+
+# Read Policy File
+$PolicyFileNameWest = 'policyfile_west.txt'
+$PolicyFileNameNorth = 'policyfile_north.txt'
+if ($whereto -eq 'westeu') {$PolicyFileName = $PolicyFileNameWest; 
+        Write-Host "Please wait, switching to Western Europe"; }
+    elseif ($whereto -eq 'northeu') {$PolicyFileName = $PolicyFileNameNorth; 
+        Write-Host "Please wait, switching to North Europe"; }
+    else { Write-Host "You did not enter a valid region"; 
+       exit; }
+
+$PSScriptRoot = "C:\Users\smanalo\Source\Repos\PingAPIMgmt\"
+
+$PolicyFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $PolicyFileName))
+
+
+Set-AzureRmApiManagementPolicy -Context $ApiMgmtContext -ApiId $switchapi.ApiId -PolicyFile $PolicyFile
+
+$Policy = Get-AzureRmApiManagementPolicy -Context $ApiMgmtContext -ApiId $switchapi.ApiId
+
+Write-Host "Current policy is: " $Policy
